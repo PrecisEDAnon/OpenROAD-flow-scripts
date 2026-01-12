d0b6f015fb3bfffb1f3e811ca446c37522ea037b
aghose <aghose@local>
2026-01-12 03:20:34 +0000

    place: optional soft placement clusters (CSV)

--- flow/scripts/global_place_skip_io.tcl
+++ flow/scripts/global_place_skip_io.tcl
@@ -7,7 +7,20 @@ if { [env_var_exists_and_non_empty FLOORPLAN_DEF] } {
 } elseif { [all_pins_placed] } {
   puts "All pins are placed. Skipping global placement without IOs"
 } else {
-  log_cmd global_placement -skip_io -density [place_density_with_lb_addon] \
+  if { [env_var_exists_and_non_empty GPL_SOFT_PLACEMENT_CLUSTERS_FILE] } {
+    set cluster_args [list -file $::env(GPL_SOFT_PLACEMENT_CLUSTERS_FILE) -best_effort]
+    append_env_var cluster_args GPL_SOFT_PLACEMENT_CLUSTERS_MIN_WEIGHT -min_weight 1
+    append_env_var cluster_args GPL_SOFT_PLACEMENT_CLUSTERS_MIN_SIZE -min_cluster_size 1
+    append_env_var cluster_args GPL_SOFT_PLACEMENT_CLUSTERS_MAX_SIZE -max_cluster_size 1
+    append_env_var cluster_args GPL_SOFT_PLACEMENT_CLUSTERS_SPLIT_LARGE -split_large_clusters 0
+    log_cmd read_soft_placement_clusters {*}$cluster_args
+  }
+
+  set global_placement_args [list -skip_io]
+  append_env_var global_placement_args GPL_RANDOM_SEED -random_seed 1
+
+  log_cmd global_placement {*}$global_placement_args \
+    -density [place_density_with_lb_addon] \
     -pad_left $::env(CELL_PAD_IN_SITES_GLOBAL_PLACEMENT) \
     -pad_right $::env(CELL_PAD_IN_SITES_GLOBAL_PLACEMENT) \
     {*}[env_var_or_empty GLOBAL_PLACEMENT_ARGS]
