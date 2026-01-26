5d03d479622f7e80479ba3ddedd00f044635a0af
PrecisEDAnon <PrecisEDAnon@proton.me>
2026-01-26 16:53:06 +0000

    flow: stop overriding GPL weight defaults
    When ORFS_ENABLE_NEW_OPENROAD=1, enable OpenROAD's new timing-driven net weights, but don't override OpenROAD's default GPL_WEIGHT_USE_ZERO_REF / keep_resize_below_overflow unless the user sets flow variables.
    
    Signed-off-by: PrecisEDAnon <PrecisEDAnon@proton.me>

--- flow/scripts/global_place.tcl
+++ flow/scripts/global_place.tcl
@@ -29,25 +29,12 @@ append_env_var global_placement_args GPL_ROUTABILITY_DRIVEN -routability_driven 
 if { $::env(GPL_TIMING_DRIVEN) } {
   lappend global_placement_args {-timing_driven}
   if { [env_var_truthy ORFS_ENABLE_NEW_OPENROAD] } {
-    # OpenROAD's new timing-driven net-weight mapping defaults to using 0-slack
-    # as the reference point (only negative-slack nets get weighted). This can
-    # be a large QoR regression for some designs, so default to the legacy
-    # slack-based reference unless explicitly overridden.
-    if { ![env_var_exists_and_non_empty GPL_WEIGHT_USE_ZERO_REF] } {
-      set ::env(GPL_WEIGHT_USE_ZERO_REF) 0
-    }
     lappend global_placement_args {-timing_driven_use_new_net_weights}
-    set gpl_keep_overflow ""
     if { [env_var_exists_and_non_empty GPL_KEEP_OVERFLOW] } {
-      set gpl_keep_overflow $::env(GPL_KEEP_OVERFLOW)
-    } elseif { [info exists ::env(PLATFORM)] && $::env(PLATFORM) eq "nangate45" } {
-      set gpl_keep_overflow 1.0
-    }
-    if { $gpl_keep_overflow ne "" } {
-      lappend global_placement_args -keep_resize_below_overflow $gpl_keep_overflow
+      lappend global_placement_args -keep_resize_below_overflow $::env(GPL_KEEP_OVERFLOW)
     }
   } else {
-    if { [info exists ::env(GPL_KEEP_OVERFLOW)] } {
+    if { [env_var_exists_and_non_empty GPL_KEEP_OVERFLOW] } {
       lappend global_placement_args -keep_resize_below_overflow $::env(GPL_KEEP_OVERFLOW)
     }
   }
